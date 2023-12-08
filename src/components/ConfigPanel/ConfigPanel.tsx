6837ef78cdb6fc99ee8864160d9206c36d664933
Nyarlathotep <anhedonia@skiff.com>
2023-12-08 18:57:43 -0500

    clean component

--- src/components/ConfigPanel/ConfigPanel.tsx
+++ src/components/ConfigPanel/ConfigPanel.tsx
@@ -1,18 +1,12 @@
-import {
-  type JSX,
-  type Component,
-  createSignal,
-  createUniqueId
-} from 'solid-js'
+import { type JSX, type Component, createSignal } from 'solid-js'
 import { Portal } from 'solid-js/web'
+
 import { useSettings } from '@features/settings/provider.tsx'
 import { saveFile } from '@features/backgroundFile/handleStorage.ts'
-
 import { GearSVG } from './gear.js'
 import * as styles from './Settings.css.js'
 
 type InputEventHandler = JSX.EventHandler<HTMLInputElement, InputEvent>
-
 interface SettingsProps {
   children?: JSX.Element
 }
@@ -20,7 +14,6 @@ interface SettingsProps {
 const Settings: Component<SettingsProps> = () => {
   const [modalStatus, setModal] = createSignal<boolean>(false)
   const [, { setBackground }] = useSettings()
-  const inputId = createUniqueId()
 
   const inputBackgroundEvent: InputEventHandler = event => {
     const { files } = event.currentTarget
@@ -36,24 +29,14 @@ const Settings: Component<SettingsProps> = () => {
     <Portal>
       <button
         onClick={() => setModal(prev => !prev)}
-        style={{
-          all: 'unset',
-          position: 'absolute',
-          top: '1em',
-          left: '1em'
-        }}
         type="button"
+        class={styles.configBtn}
       >
         {GearSVG}
       </button>
 
       <dialog class={styles.dialog} open={modalStatus()}>
-        <input
-          type="file"
-          accept="image/*"
-          id={inputId}
-          onInput={inputBackgroundEvent}
-        />
+        <input type="file" accept="image/*" onInput={inputBackgroundEvent} />
       </dialog>
     </Portal>
   )
--- src/components/ConfigPanel/Settings.css.ts
+++ src/components/ConfigPanel/Settings.css.ts
@@ -7,10 +7,14 @@ export const showModalBtn = style({
   left: '1em'
 })
 
-export const configBtn = style({})
+export const configBtn = style({
+  all: 'unset',
+  position: 'absolute',
+  top: '1em',
+  left: '1em'
+})
 
 export const dialog = style({
-  // position: 'fixed',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)'
