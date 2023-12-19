5f9384272a4a98b0e58387cbbdbfce354f47d96a
Nyarlathotep <anhedonia@skiff.com>
2023-12-19 18:45:33 -0500

    chore: move index.html to root directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
-import { resolve } from 'path'
 import TSConfigPath from 'vite-tsconfig-paths'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-  root: 'src',
-  publicDir: resolve(__dirname, 'public'),
-  build: {
-    target: 'esnext',
-    outDir: resolve(__dirname, 'dist')
-  },
   server: { port: 8080 },
 
   plugins: [vanillaExtractPlugin(), TSConfigPath(), solid()]
